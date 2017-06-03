8c5cd5654d340bd879d98a0876755fecc7c0c3da
Charles Lovering <cjlovering@wpi.edu>
2017-06-03 16:13:00 -0700

    updated readme on how to deploy

--- README.md
+++ README.md
@@ -21,12 +21,46 @@ Install mongodb to load json data directly into db
 brew install mongodb
 ```
 
-Load JSON data into db
+Load JSON data into db locally. Edit load_data to not deploy.
 
 ```
-    mongoimport -h localhost:3001 --db meteor --collection tweets --type json --file exampletweets.json --jsonArray	
+./load_data.sh
 ```
-
-
 
 To run, enter `meteor` and view site at `http://localhost:3000`
+
+Deploying site. Set up heroku and mongodb.
+
+```
+# set up heroku
+heroku login
+heroku apps:create foobar
+
+# set buildpack for meteor
+heroku buildpacks:set https://github.com/AdmitHub/meteor-buildpack-horse.git
+
+# create mLab (mongodb) instance. note: you will need to verify heroku account
+heroku addons:create mongolab:sandbox
+
+# find MONGODB_URI
+heroku config | grep MONGODB_URI
+
+# set config vars for heroku/meteor
+heroku config:add MONGO_URL=<MONGODB_URI value>
+heroku config:add ROOT_URL=https://foobar.herokuapp.com
+
+# set config vars for loading data
+#  look at MONGODB_URI and parse as following:
+#  mongodb://<dbUser>:<dbPassword>@<service>.com:<port>/<databaseName>
+heroku config:set MONGO_USER=<dbUser>
+heroku config:set MONGO_NAME=<databaseName>
+heroku config:set MONGO_PORT=<port>
+heroku config:set MONGO_HOST=<service>
+heroku config:set MONGO_PASS=<dbPassword>
+
+# push app
+git push heroku master
+
+# load data
+./load_data.sh
+```
--- load_data.sh
+++ load_data.sh
@@ -1,16 +1,19 @@
 # update data!
+DEPLOY = true
 
-## get conf variables 
-heroku config -s
+if [ $DEPLOY ]
+    ## get conf variables 
+    heroku config -s
 
-## delete old data
-mongo $MONGO_HOST:$MONGO_PORT/$MONGO_NAME -u $MONGO_USER -p $MONGO_PASS --eval "db.dropDatabase()"
+    ## delete old data
+    mongo $MONGO_HOST:$MONGO_PORT/$MONGO_NAME -u $MONGO_USER -p $MONGO_PASS --eval "db.dropDatabase()"
 
-## import new data
-mongoimport -h $MONGO_HOST:$MONGO_PORT --db $MONGO_NAME --collection projects -u $MONGO_USER -p $MONGO_PASS --type json --file server/data/projects.json --jsonArray
-mongoimport -h $MONGO_HOST:$MONGO_PORT --db $MONGO_NAME --collection professional -u $MONGO_USER -p $MONGO_PASS --type json --file server/data/professional.json --jsonArray
-mongoimport -h $MONGO_HOST:$MONGO_PORT --db $MONGO_NAME --collection reading -u $MONGO_USER -p $MONGO_PASS --type json --file server/data/reading.json --jsonArray
-mongoimport -h $MONGO_HOST:$MONGO_PORT --db $MONGO_NAME --collection research -u $MONGO_USER -p $MONGO_PASS --type json --file server/data/research.json --jsonArray
+    ## import new data
+    mongoimport -h $MONGO_HOST:$MONGO_PORT --db $MONGO_NAME --collection projects -u $MONGO_USER -p $MONGO_PASS --type json --file server/data/projects.json --jsonArray
+    mongoimport -h $MONGO_HOST:$MONGO_PORT --db $MONGO_NAME --collection professional -u $MONGO_USER -p $MONGO_PASS --type json --file server/data/professional.json --jsonArray
+    mongoimport -h $MONGO_HOST:$MONGO_PORT --db $MONGO_NAME --collection reading -u $MONGO_USER -p $MONGO_PASS --type json --file server/data/reading.json --jsonArray
+    mongoimport -h $MONGO_HOST:$MONGO_PORT --db $MONGO_NAME --collection research -u $MONGO_USER -p $MONGO_PASS --type json --file server/data/research.json --jsonArray
+fi
 
 ## import local data for dev
 mongo meteor --host localhost:3001 --eval "db.dropDatabase()"
