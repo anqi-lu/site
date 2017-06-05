63f2e04fb910cbd4c86d97aea74c22035d0fe5c6
Charles Lovering <cjlovering@wpi.edu>
2017-06-05 00:11:31 -0700

    trying to make things more general

--- imports/api/collections.js
+++ imports/api/collections.js
@@ -2,10 +2,15 @@ import { Mongo } from 'meteor/mongo';
 
 import LU from './resources';
 
-const First = LU ? new Mongo.Collection('a_projects') : new Mongo.Collection('c_projects');
-const Second = LU ? new Mongo.Collection('a_research') : new Mongo.Collection('c_research');
-const Third = LU ? new Mongo.Collection('a_reading') : new Mongo.Collection('c_reading');
-const Fourth = LU ? new Mongo.Collection('a_professional') : new Mongo.Collection('c_professional');
+// const First = LU ? new Mongo.Collection('a_projects') : new Mongo.Collection('c_projects');
+// const Second = LU ? new Mongo.Collection('a_research') : new Mongo.Collection('c_research');
+// const Third = LU ? new Mongo.Collection('a_reading') : new Mongo.Collection('c_reading');
+// const Fourth = LU ? new Mongo.Collection('a_professional') : new Mongo.Collection('c_professional');
+
+let First = new Mongo.Collection('c_projects');
+let Second = new Mongo.Collection('c_research');
+let Third = new Mongo.Collection('c_reading');
+let Fourth = new Mongo.Collection('c_professional');
 
 export { First, Second, Third, Fourth };
 
