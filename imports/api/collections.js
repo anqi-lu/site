6b6c0460f3cd5e7440a77aa89538f5da22be9dbe
Charles Lovering <cjlovering@wpi.edu>
2017-06-05 00:08:15 -0700

    trying to make things more general

--- imports/api/collections.js
+++ imports/api/collections.js
@@ -2,10 +2,10 @@ import { Mongo } from 'meteor/mongo';
 
 import LU from './resources';
 
-const First = new Mongo.Collection('c_projects');
-const Second = new Mongo.Collection('c_research');
-const Third = new Mongo.Collection('c_reading');
-const Fourth = new Mongo.Collection('c_professional');
+const First = LU ? new Mongo.Collection('a_projects') : new Mongo.Collection('c_projects');
+const Second = LU ? new Mongo.Collection('a_research') : new Mongo.Collection('c_research');
+const Third = LU ? new Mongo.Collection('a_reading') : new Mongo.Collection('c_reading');
+const Fourth = LU ? new Mongo.Collection('a_professional') : new Mongo.Collection('c_professional');
 
 export { First, Second, Third, Fourth };
 
